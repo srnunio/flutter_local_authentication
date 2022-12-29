b9d5ed535ac13308885f62a3970764059395c6ac
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2022-12-29 22:21:33 +0100

    add ui by option auth

---
+++ lib/screens/auth_option_component.dart
@@ -0,0 +1,47 @@
+import 'package:custom_switch_widget/custom_switch_widget.dart';
+import 'package:flutter/material.dart';
+
+class AuthOptionComponent extends StatelessWidget {
+  final String title;
+  final bool isAvailable;
+  final IconData iconData;
+  final CustomSwitchController controller;
+  final Function(bool) onChange;
+
+  AuthOptionComponent(
+      {required this.title,
+      required this.iconData,
+      required this.isAvailable,
+      required this.controller,
+      required this.onChange});
+
+  void onAction(bool value) {
+    if (!isAvailable) return;
+    onChange(value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            color: Colors.white12, borderRadius: BorderRadius.circular(16.0)),
+        child: ListTile(
+          leading: Icon(
+            iconData,
+            color: Colors.white,
+          ),
+          title: Text(
+            title,
+            style: TextStyle(color: Colors.white),
+          ),
+          subtitle: isAvailable
+              ? null
+              : const Text(
+                  "Unavailable",
+                  style: TextStyle(color: Colors.red, fontSize: 11.0),
+                ),
+          trailing:
+              CustomSwitchWidget(controller: controller, onChange: onAction),
+        ));
+  }
+}
